e4cf341b221d68d00a95d77a0ecffa48f52db34c
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-18 19:22:06 +0200

    Add file scanner

--- Duplicate File Handler (Python)/task/handler.py
+++ Duplicate File Handler (Python)/task/handler.py
@@ -1,13 +1,22 @@
 import sys
+import os
 
 
 def validate_input():
     if len(sys.argv) != 2:
         print("Directory is not specified")
+        sys.exit()
 
 
 def main():
     validate_input()
+    scan_for_files(sys.argv[1])
+
+
+def scan_for_files(root):
+    for root, dirs, files in os.walk(root):
+        for name in files:
+            print(os.path.join(root, name))
 
 
 if __name__ == "__main__":
