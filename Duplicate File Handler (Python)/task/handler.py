c92d0b60e66bf904f90fbdaf03c6dc24be6968fa
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-19 11:39:39 +0200

    Find duplicate files by file digest

--- Duplicate File Handler (Python)/task/handler.py
+++ Duplicate File Handler (Python)/task/handler.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import hashlib
+from collections import defaultdict
 from os.path import join, getsize, splitext
 
 
@@ -9,6 +11,8 @@ class DuplicateFileHandler:
         self.file_format = None
         self.sorting_option = None
         self.files = dict()
+        # self.hash_table = defaultdict(set)
+        self.size_hash_table = defaultdict(dict)
 
     def scan_files(self):
         for root, dirs, files in os.walk(self.root_directory):
@@ -22,12 +26,24 @@ class DuplicateFileHandler:
                         continue
 
                 if size in self.files:
-                    self.files[size].append(filepath)
+                    self.files[size].add(filepath)
                 else:
-                    self.files[size] = []
-                    self.files[size].append(filepath)
+                    self.files[size] = set()
+                    self.files[size].add(filepath)
 
         return self.files
+
+    def compute_hashes(self):
+        for size, filepaths in self.files.items():
+            hash_table = defaultdict(set)
+            if len(filepaths) > 1:
+                for filepath in filepaths:
+                    with open(filepath, "rb") as f:
+                        digest = hashlib.file_digest(f, "md5")
+                    file_hash = digest.hexdigest()
+                    hash_table[file_hash].add(filepath)
+                self.size_hash_table[size] = hash_table
+        return self.size_hash_table
 
 
 class DuplicateFileHandlerApplication:
@@ -41,9 +57,16 @@ class DuplicateFileHandlerApplication:
         while (sorting_option := int(input("Enter a sorting option: "))) not in [1, 2]:
             print("Wrong option")
         self.sorting_option = sorting_option
-        self.print_duplicates()
+        self.print_equal_sizes()
 
-    def print_duplicates(self):
+        while (
+            check_for_duplicates := input("\nCheck for duplicates? [yes/no]: ")
+        ) not in ["yes", "no"]:
+            print("Wrong option")
+        if check_for_duplicates == "yes":
+            self.print_equal_hashes()
+
+    def print_equal_sizes(self):
         is_descending = True if self.sorting_option == 1 else False
         files = self.handler.scan_files()
         for size, filepaths in sorted(files.items(), reverse=is_descending):
@@ -51,6 +74,19 @@ class DuplicateFileHandlerApplication:
                 print(f"\n{size} bytes")
                 for filepath in filepaths:
                     print(filepath)
+
+    def print_equal_hashes(self):
+        is_descending = True if self.sorting_option == 1 else False
+        size_hash_table = self.handler.compute_hashes()
+        counter = 1
+        for size, hash_table in sorted(size_hash_table.items(), reverse=is_descending):
+            print(f"\n{size} bytes")
+            for hash_, filepaths in hash_table.items():
+                if len(filepaths) > 1:
+                    print(f"Hash: {hash_}")
+                    for filepath in filepaths:
+                        print(f"{counter}. {filepath}")
+                        counter += 1
 
 
 def main():
