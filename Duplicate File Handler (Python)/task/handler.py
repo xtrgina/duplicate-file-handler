bf05b95a14845d785e5d44c9297b4bbb4d4855fa
Marek Trgina <32020987+xtrgina@users.noreply.github.com>
2023-04-19 09:31:19 +0200

    Refactor UI into separate class

--- Duplicate File Handler (Python)/task/handler.py
+++ Duplicate File Handler (Python)/task/handler.py
@@ -4,12 +4,6 @@ from os.path import join, getsize, splitext
 
 
 class DuplicateFileHandler:
-    @classmethod
-    def validate_input_args(cls):
-        if len(sys.argv) != 2:
-            print("Directory is not specified")
-            sys.exit()
-
     def __init__(self, root_directory):
         self.root_directory = root_directory
         self.file_format = None
@@ -33,28 +27,38 @@ class DuplicateFileHandler:
                     self.files[size] = []
                     self.files[size].append(filepath)
 
+        return self.files
+
+
+class DuplicateFileHandlerApplication:
+    def __init__(self, root_directory):
+        self.handler = DuplicateFileHandler(root_directory)
+        self.sorting_option = None
+
+    def execute(self):
+        self.handler.file_format = input("Enter file format: ")
+        print("Size sorting options:\n1. Descending\n2. Ascending\n")
+        while (sorting_option := int(input("Enter a sorting option: "))) not in [1, 2]:
+            print("Wrong option")
+        self.sorting_option = sorting_option
+        self.print_duplicates()
+
     def print_duplicates(self):
         is_descending = True if self.sorting_option == 1 else False
-        for size, filepaths in sorted(self.files.items(), reverse=is_descending):
+        files = self.handler.scan_files()
+        for size, filepaths in sorted(files.items(), reverse=is_descending):
             if len(filepaths) > 1:
                 print(f"\n{size} bytes")
                 for filepath in filepaths:
                     print(filepath)
 
-    def display_UI(self):
-        self.file_format = input("Enter file format: ")
-        print("Size sorting options:\n1. Descending\n2. Ascending\n")
-        while (sorting_option := int(input("Enter a sorting option: "))) not in [1, 2]:
-            print("Wrong option")
-        self.sorting_option = sorting_option
-
 
 def main():
-    DuplicateFileHandler.validate_input_args()
-    handler = DuplicateFileHandler(root_directory=sys.argv[1])
-    handler.display_UI()
-    handler.scan_files()
-    handler.print_duplicates()
+    if len(sys.argv) != 2:
+        print("Directory is not specified")
+        sys.exit()
+    app = DuplicateFileHandlerApplication(root_directory=sys.argv[1])
+    app.execute()
 
 
 if __name__ == "__main__":
